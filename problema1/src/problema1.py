8f2a57fc4c88372c68271b15337512220aff89cd
Damaris <damarismjuares@gmail.com>
2025-09-22 16:39:55 -0300

    problema1

--- problema1/src/problema1.py
+++ problema1/src/problema1.py
@@ -1,4 +1,4 @@
-def busqbin(lista, inicio, final):
+def problema1(lista, inicio, final):
 
     # caso base: si inicio es mayor a final, la condición no existe
     if inicio > final:  
@@ -10,16 +10,16 @@ def busqbin(lista, inicio, final):
         return mitad
     
     if lista[mitad] > mitad:
-        result = busqbin(lista, inicio, mitad - 1)
+        result = problema1(lista, inicio, mitad - 1)
         if result != "no existe i":
             return result
-        return busqbin(lista, lista[mitad] + 1, final)
+        return problema1(lista, lista[mitad] + 1, final)
     
     else:
-        result = busqbin(lista, inicio, mitad - 1)
+        result = problema1(lista, inicio, mitad - 1)
         if result != "no existe i":
             return result
-        return busqbin(lista, mitad + 1, final)
+        return problema1(lista, mitad + 1, final)
 
-print(busqbin([0,3,4,5,6,9], 0, 5))
+print(problema1([0,3,4,5,6,9], 0, 5))
 
